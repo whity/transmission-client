51b1c5abd5295d923a9ab956ac9b0a21229c1867
André Brás <whity@andre-brass-macbook.local>
2010-05-26 20:55:02 +0100

    handling attributes files and wanted   - files, the array is convert for an array of TorrentFile objects   - wanted, each position of the array is merged to the TorrentFile object

---
+++ examples/files-example.rb
@@ -0,0 +1,3 @@
+require(File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'transmission-client')))
+t = Transmission::Client.new('127.0.0.1', 9091, 'transmission', 'transmission')
+puts(t.torrents[0].files()[0].name)
